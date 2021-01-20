928b55800884be8b150c69c4d570697095629fe9
--add <--add>
2021-01-20 15:49:07 +0530

    setup spatie and create the roles and permission

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+        //Create new role using Spatie Role
+        // Role::Create(["name"=>"student"]);
+        // Role::Create(["name"=>"educator"]);
+        // Role::Create(["name"=>"company"]);
+
+
+        //Create new Permission using Spatie Permission
+        // Permission::Create(['name' => 'edit articles']);
+
+      
+        $permission = Permission::findById(1);
+
+
+        $role = Role::findById(1);
+
+        //give permission to role    
+        // $role->givePermissionTo($permission);
+
+
+        //remove permission 
+        // $permission->removeRole($role);
+
+
+
+        return view('home');
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -17,7 +19,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'fist_name',
+        'last_name',
         'email',
         'password',
     ];
